f1a1e0e0fd8b3f0a145f51b4167d53fef516e7ec
Bakai Taalaibek <bakaitaalaibek@gmail.com>
2017-01-17 20:50:41 +0600

    First git commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,49 @@
+class ArticlesController < ApplicationController
+	def new
+		@article = Article.new
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash[:notice]
+		redirect_to articles_path
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if
+			@article.update(article_params)
+			flash[:notice] = "Article updated successfully"
+		else
+			render 'edit'
+		end
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+		flash[:notice] = "Aticle was created successfully"
+		redirect_to article_path(@article)
+	else 
+		render 'new'
+	end
+end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def index
+		@articles = Article.all
+	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title,:description)
+		end
+end 
